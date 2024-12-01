dc7759e32cebe5110bcaa6776f714d7756eae650
Stef Robbe <stef.robbe@protonmail.com>
2024-12-01 10:54:27 +0100

    feat: day 1, part 1

--- 2024/01.rs
+++ 2024/01.rs
@@ -1,18 +1,41 @@
 use advent::prelude::*;
 
-fn parse_input(input: &str) -> Vec<i64> {
-    todo!("parsing")
+// because advent::part() does not account for tuples(?)
+type Lists = (Vec<i64>, Vec<i64>);
+
+fn parse_input(input: &str) -> Lists {
+    let mut list1 = vec![];
+    let mut list2 = vec![];
+    for l in input.split('\n') {
+        if l == "" {
+            continue;
+        }
+        let mut both = l.split_whitespace();
+        list1.push(both.next().unwrap().parse::<i64>().unwrap());
+        list2.push(both.next().unwrap().parse::<i64>().unwrap());
+    }
+
+    list1.sort();
+    list2.sort();
+    (list1, list2)
 }
 
-fn default_input() -> Vec<i64> {
+fn default_input() -> Lists {
     parse_input(include_input!(2024 / 01))
 }
 
-fn part1(input: Vec<i64>) -> i64 {
-    todo!("part 1")
+fn part1(lists: Lists) -> i64 {
+    let mut total = 0i64;
+    let (list1, list2) = lists;
+
+    for i in 0..list1.len() {
+        total += (list1[i] - list2[i]).abs();
+    }
+
+    total
 }
 
-fn part2(input: Vec<i64>) -> i64 {
+fn part2(lists: Lists) -> i64 {
     todo!("part 2")
 }
 
@@ -22,8 +45,23 @@ fn main() {
 }
 
 #[test]
+fn example() {
+    let lists = parse_input(
+        "3   4
+4   3
+2   5
+1   3
+3   9
+3   3
+",
+    );
+    assert_eq!(part1(lists.clone()), 11);
+    // assert_eq!(part2(lists), 26);
+}
+
+#[test]
 fn default() {
-    let input = default_input();
-    assert_eq!(part1(input.clone()), 1);
-    assert_eq!(part2(input), 2);
+    let lists = default_input();
+    assert_eq!(part1(lists.clone()), 936063);
+    // assert_eq!(part2(lists), 2);
 }
